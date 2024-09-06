f1b221a6b613e10108f92565707f84135ba21fa2
reivaJSnevet <javier.diaz.marin@est.una.ac.cr>
2024-09-06 10:49:13 -0600

    Add bookmark and remove bookmark functionality to company controller

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -8,5 +8,8 @@ companyRouter.get("/companies", companyController.getAll);
 companyRouter.get("/companies/:id", companyController.getById);
 companyRouter.put("/companies/:id", companyController.update);
 companyRouter.delete("/companies/:id", companyController.delete);
+companyRouter.post("/companies/:companyId/projects/:projectId", companyController.addBookmark);
+companyRouter.delete("/companies/:companyId/projects/:projectId", companyController.removeBookmark);
+
 
 export default companyRouter;
