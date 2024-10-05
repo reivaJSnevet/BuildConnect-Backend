b7cc66acd49d2f5a20dc7dea4c507219ef9aaf2a
reivaJSnevet <javier.diaz.marin@est.una.ac.cr>
2024-10-04 19:22:04 -0600

    Refactor register route at auth to register-user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,7 +4,7 @@ import authController from "../controller/authController.js";
 const authRouter = Router();
 
 authRouter.post("/login", authController.login);
-authRouter.post("/register", authController.registerUser);
+authRouter.post("/register-user", authController.registerUser);
 authRouter.post("/register-company", authController.registerCompany);
 authRouter.get("/verify-email/:token", authController.verifyEmail);
 
