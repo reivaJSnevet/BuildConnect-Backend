03bca23464dd449eaf6b11193afb5a4799c3d9fe
reivaJSnevet <javier.diaz.marin@est.una.ac.cr>
2024-11-08 18:05:46 -0600

    Add ratings functionality for owners: implement get, add, and update ratings endpoints

--- src/routes/ownerRoutes.js
+++ src/routes/ownerRoutes.js
@@ -1,15 +1,16 @@
-import { Router } from "express";
-import ownerController from "../controller/ownerController.js";
+import { Router } from 'express';
+import ownerController from '../controller/ownerController.js';
 
 const ownerRouter = Router();
 
-ownerRouter.post("/owners", ownerController.create);
-ownerRouter.get("/owners", ownerController.getAll);
-ownerRouter.get("/owners/:id", ownerController.getById);
-ownerRouter.put("/owners/:id", ownerController.update);
-ownerRouter.delete("/owners/:id", ownerController.delete);
+ownerRouter.post('/owners', ownerController.create);
+ownerRouter.get('/owners', ownerController.getAll);
+ownerRouter.get('/owners/:id', ownerController.getById);
+ownerRouter.put('/owners/:id', ownerController.update);
+ownerRouter.delete('/owners/:id', ownerController.delete);
 
-ownerRouter.post("/owners/:id/Rating", ownerController.addRating);
-ownerRouter.put("/owners/:id/Rating", ownerController.updateRating);
+ownerRouter.get('/owners/:id/Rating', ownerController.getRatings);
+ownerRouter.post('/owners/:id/Rating', ownerController.addRating);
+ownerRouter.put('/owners/:id/Rating', ownerController.updateRating);
 
 export default ownerRouter;
